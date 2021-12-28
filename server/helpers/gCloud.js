1a9c6d139cf097dbc70ede2562f8eed0cad15a76
Archana Agrawal <69050324+archana-agrawal@users.noreply.github.com>
2021-12-29 01:12:36 +0530

    logs

--- server/helpers/gCloud.js
+++ server/helpers/gCloud.js
@@ -4,12 +4,10 @@ const uploadFile = require("../utils/gCloud").uploadFile;
 
 const gCloudUrl = async (localPath, folderName) => {
 	const filePath = localPath.replace('\\', '/');
-	const filedestPath = folderName + filePath.substring(filePath.lastIndexOf('\\') + 1);
-	console.log("fild: ", filedestPath);
-	console.log("name: ", filePath.substring(filePath.lastIndexOf('/') + 1));
+	const filedestPath = folderName + filePath.substring(filePath.lastIndexOf('/') + 1);
 	let fileUrl = uploadFile(filePath, filedestPath);
 	fileUrl = await Promise.all([fileUrl]);
-	fs.unlinkSync(localDestFilePath);
+	fs.unlinkSync(filePath);
 	return fileUrl[0];
 }
 
