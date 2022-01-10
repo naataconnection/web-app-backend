72fefc8c84025b23fe33233da1b6b9e8d8481598
tushar <tbajaj@iitg.ac.in>
2022-01-10 21:32:17 +0530

    added config for headers

--- server/app.js
+++ server/app.js
@@ -15,6 +15,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'x-www-form-urlencoded, Origin, X-Requested-With, Content-Type, Accept, Authorization, *');
+  if (req.method === 'OPTIONS') {
+     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
+     res.setHeader('Access-Control-Allow-Credentials', true);
+     return res.status(200).json({});
+  }
+  next();
+});
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
+app.use(bodyParser.json({
+  limit: '50mb',
+  extended: true
+}));
+
 // Passport Configuration
 require("./config/passport")(passport);
 
